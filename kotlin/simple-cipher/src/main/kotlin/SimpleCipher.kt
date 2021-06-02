ed2094c7c8265e8963d28936fa913ca128f28dae
Adrian Spanu <adri@nspanu.ca>
2021-06-01 23:49:05 -0500

    Adding gitignore and solving the simple cipher.

--- kotlin/simple-cipher/src/main/kotlin/SimpleCipher.kt
+++ kotlin/simple-cipher/src/main/kotlin/SimpleCipher.kt
@@ -1,12 +1,43 @@
-data class Cipher(val todo: Nothing) {
+import kotlin.random.Random
 
-    // TODO: Implement proper constructor
+data class Cipher(val key: String = createRandomKey()) {
+
+    init {
+        if (key.contains(Regex("[^a-z]"))) {
+            throw IllegalArgumentException("Key cannot contain non lower case characters.")
+        }
+        if (key.isBlank()) {
+            throw IllegalArgumentException("Key cannot be empty.")
+        }
+    }
 
     fun encode(s: String): String {
-        TODO("Implement this function to complete the task")
+        // For every item encoding string, move the characters by the offset given by the position in the
+        // alphabet. I.e. -> a == 0, b == 1
+        var encodedMessage = ""
+        for (i in s.indices) {
+            val newIndex = (ALPHABET.indexOf(s[i].toString()) + ALPHABET.indexOf(key[i % key.length].toString())) % ALPHABET.size
+            encodedMessage += ALPHABET[newIndex]
+        }
+        return encodedMessage
     }
 
     fun decode(s: String): String {
-        TODO("Implement this function to complete the task")
+        var decodedMessage = ""
+        for (i in s.indices) {
+            val newIndex = ((ALPHABET.indexOf(s[i].toString()) - ALPHABET.indexOf(key[i % key.length].toString())) + ALPHABET.size) % ALPHABET.size
+            decodedMessage += ALPHABET[newIndex]
+        }
+        return decodedMessage
     }
+}
+
+private val ALPHABET = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ")
+
+private fun createRandomKey(): String {
+    var key = ""
+    for (i in 1..100) {
+        key += ALPHABET[Random.nextInt(ALPHABET.size)]
+    }
+    return key
 }
